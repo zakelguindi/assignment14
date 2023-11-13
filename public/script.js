801dfc683a8d54bbdef2b28b596fa08d4bf4c14a
zakelguindi <89213113+zakelguindi@users.noreply.github.com>
2023-11-13 00:24:15 -0500

    pushing submission

--- public/script.js
+++ public/script.js
@@ -9,38 +9,124 @@ const showTeams = async () => {
     return; 
   }
 
+  // teamsDiv.innerHTML = ""; 
+
   let num = 1; 
 
   TeamsJSON.forEach((team) => {
     const section = document.createElement("section"); 
     section.classList.add("team"); 
-    teamsDiv.append(section); 
 
-    const name = document.createElement("h1"); 
-    name.innerHTML = num+". "+team.name; 
-    num++;
-    section.append(name); 
+    // section.append(name); 
 
-    const city = document.createElement("h3"); 
-    city.innerHTML = team.city; 
-    section.append(city);
+  //   const city = document.createElement("h3"); 
+  //   city.innerHTML = team.city; 
+  //   section.append(city);
 
-    const arena = document.createElement("p"); 
-    arena.innerHTML = "Arena Name: "+team.arena; 
-    section.append(arena); 
+  //   const arena = document.createElement("p"); 
+  //   arena.innerHTML = "Arena Name: "+team.arena; 
+  //   section.append(arena); 
 
-    const bestPlayer = document.createElement("p"); 
-    bestPlayer.innerHTML = "Best Player: "+team.bestPlayer; 
-    section.append(bestPlayer); 
+  //   const bestPlayer = document.createElement("p"); 
+  //   bestPlayer.innerHTML = "Best Player: "+team.bestPlayer; 
+  //   section.append(bestPlayer); 
 
-    const titlesWon = document.createElement("p"); 
-    titlesWon.innerHTML = "Titles Won: "+team.titlesWon; 
-    section.append(titlesWon); 
+  //   const titlesWon = document.createElement("p"); 
+  //   titlesWon.innerHTML = "Titles Won: "+team.titlesWon; 
+  //   section.append(titlesWon); 
 
-    const startersDiv = document.createElement("div"); 
-    const startersDivTitle = document.createElement("h3"); 
-    startersDivTitle.innerHTML = "Starting 5"; 
-    startersDiv.append(startersDivTitle);
+  //   const startersDiv = document.createElement("div"); 
+  //   const startersDivTitle = document.createElement("h3"); 
+  //   startersDivTitle.innerHTML = "Starting 5"; 
+  //   startersDiv.append(startersDivTitle);
+
+  //   let i = 1; 
+  //   let position = "PG"; 
+  //   team.starting5.forEach((player) => {
+  //     if(i == 1) {
+  //       position = "PG"
+  //     } else if(i ==2) {
+  //       position = "SG"; 
+  //     } else if(i == 3) {
+  //       position = "SF"; 
+  //     } else if(i == 4) {
+  //       position = "PF"; 
+  //     } else {
+  //       position = "C"
+  //     }
+  //     const thisPlayer = document.createElement("ul"); 
+  //     thisPlayer.innerHTML = position + ": " + player; 
+  //     i++; 
+  //     startersDiv.append(thisPlayer);
+  // });
+
+  // startersDiv.classList.add("starting5list"); 
+  // section.append(startersDiv); 
+
+  const a = document.createElement("a"); 
+  a.href = "#"; 
+  section.append(a); 
+
+  const name = document.createElement("h1"); 
+  name.innerHTML = num+". "+team.name; 
+  num++;
+  a.append(name); 
+
+  a.onclick = (e) => {
+    e.preventDefault(); 
+    display(team); 
+  }
+
+  teamsDiv.append(section); 
+  
+
+
+
+  // return section;
+
+  });
+};
+
+
+
+const getTeams = async() => {
+  try {
+    return (await fetch("https://assignment14-242.onrender.com/api/nbateams")).json();
+  } catch(error) {
+    console.log("error retrieving render json"); 
+    return ""; 
+  }
+};
+
+const display = (team) => {
+  const details = document.getElementById("displayTeam");
+  details.innerHTML = ""; 
+  // console.log(team.name);
+
+  const h3 = document.createElement("h3"); 
+  h3.innerHTML = team.name; 
+  details.append(h3); 
+
+  const city = document.createElement("p"); 
+  city.innerHTML = team.city;
+  details.append(city); 
+
+  const arena = document.createElement("p"); 
+  arena.innerHTML = team.arena; 
+  details.append(arena); 
+
+  const bestplayer = document.createElement("p"); 
+  bestplayer.innerHTML = team.bestPlayer; 
+  details.append(bestplayer);
+
+  const titlesWon = document.createElement("p"); 
+  titlesWon.innerHTML = team.titlesWon; 
+  details.append(titlesWon); 
+
+  const startersDiv = document.createElement("div"); 
+  const startersDivTitle = document.createElement("h3"); 
+  startersDivTitle.innerHTML = "Starting 5"; 
+  startersDiv.append(startersDivTitle);
 
     let i = 1; 
     let position = "PG"; 
@@ -63,43 +149,32 @@ const showTeams = async () => {
   });
 
   startersDiv.classList.add("starting5list"); 
-  section.append(startersDiv); 
+  details.append(startersDiv); 
 
-  return section;
-
-  });
-};
-
-
-
-const getTeams = async() => {
-  try {
-    return (await fetch("https://assignment14-242.onrender.com/api/nbateams")).json();
-  } catch(error) {
-    console.log("error retrieving render json"); 
-    return ""; 
-  }
 };
 
 const addEditTeam = async(e) => {
   e.preventDefault(); 
   const form = document.getElementById("add-edit-team-form"); 
   const formData = new FormData(form); 
+  formData.append("staring5", getStarting5());
   let response; 
 
-  console.log("hello!"); 
+  // console.log("hello!"); 
   
+  //new team
   if(form._id.value == -1) {
     formData.delete("_id"); 
-    formData.append("starting5", getStarting5());
-
+    // formData.append("starting5", getStarting5());
     console.log(...formData);
 
     response = await fetch("/api/nbateams", {
-      method: "POST", 
-      body: formData
+      method:"POST", 
+      body:formData,
     });
   }
+
+  console.log(response.status);
 
   //if here- pull is successful 
   if(response.status != 200) {
@@ -107,17 +182,19 @@ const addEditTeam = async(e) => {
   }
 
   response = await response.json(); 
-  resetForm(); 
-  document.querySelector(".dialog").classList.add("transparent"); 
+  ResetForm(); 
+  // document.querySelector(".dialog").classList.add("transparent"); 
   showTeams(); 
 };
 
 const getStarting5 = () => {
+  //this works 
   const inputs = document.querySelectorAll("#player-boxes input");
   let players = []; 
 
   inputs.forEach((input) => {
     players.push(input.value); 
+    // console.log(input.value); 
   });
 
   return players; 
@@ -132,9 +209,9 @@ const ResetForm = () => {
 
 const showHideAdd = (e) => {
   e.preventDefault(); 
-  document.querySelector(".dialog").classList.remove("transparent"); 
+  // document.querySelector(".dialog").classList.remove("transparent"); 
   document.getElementById("add-edit-title").innerHTML = "Add Team"; 
-  resetForm(); 
+  ResetForm(); 
 };
 
 const addPlayer = (e) => {
@@ -149,8 +226,10 @@ window.onload = () => {
   showTeams();
   document.getElementById("add-link").onclick = showHideAdd; 
   document.getElementById("add-edit-team-form").onsubmit = addEditTeam; 
-  // document.querySelector(".close").onclick = () => {
 
-  // }
+  document.querySelector(".close").onclick = () => {
+    document.querySelector(".dialog").classList.add("transparent");
+  };
+
   document.getElementById("add-player").onclick = addPlayer; 
 } 
--- server.js
+++ server.js
@@ -13,9 +13,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/api/nbateams", (req, res) => {
-  const nbateams = []; 
+// app.get("/api/nbateams", (req, res) => {
+  let nbateams = []; 
   nbateams[0] = {
+    _id:1,
     name: "Philadelphia 76ers", 
     city: "Philadelphia, Pennsylvania", 
     arena: "Wells Fargo Center", 
@@ -24,6 +25,7 @@ app.get("/api/nbateams", (req, res) => {
     starting5: ["Tyrese Maxey", "De'Anthony Melton", "Kelly Oubre Jr." ,"Tobias Harris", "Joel Embiid"]
   };
   nbateams[1] = {
+    _id:2,
     name: "Boston Celtics", 
     city: "Boston, Massachussetts", 
     arena: "TD Garden", 
@@ -32,6 +34,7 @@ app.get("/api/nbateams", (req, res) => {
     starting5: ["Jrue Holiday", "Derrick White", "Jaylen Brown", "Jayson Tatum", "Kristaps Porzingis"]
   }; 
   nbateams[2] = {
+    _id:3,
     name: "Los Angeles Lakers", 
     city: "Los Angeles, California", 
     arena: "Crypto.com Arena", 
@@ -40,6 +43,7 @@ app.get("/api/nbateams", (req, res) => {
     starting5: ["D'Angelo Russell", "Gabe Vincent", "Austin Reaves", "LeBron James", "Anthony Davis"]
   }; 
   nbateams[3] = {
+    _id:4,
     name: "Miami Heat", 
     city: "Miami, Florida", 
     arena: "American Airlines Center", 
@@ -48,6 +52,7 @@ app.get("/api/nbateams", (req, res) => {
     starting5: ["Kyle Lowry", "Tyler Herro", "Jimmy Butler" ,"Haywood Highsmith", "Bam Adebayo"]
   };
   nbateams[4] = {
+    _id:5,
     name: "Chicago Bulls" , 
     city: "Chicago, Illinois", 
     arena: "United Center", 
@@ -56,6 +61,7 @@ app.get("/api/nbateams", (req, res) => {
     starting5: ["Coby White", "Zach LaVine", "DeMar DeRozan" , "Torrey Craig", "Nikola Vucevic"]
   };
   nbateams[5] = {
+    _id:6,
     name: "Golden State Warriors", 
     city: "San Francisco, California", 
     arena: "Chase Center", 
@@ -63,9 +69,9 @@ app.get("/api/nbateams", (req, res) => {
     titlesWon: 4, 
     starting5: ["Steph Curry", "Klay Thompson", "Andrew Wiggins", "Draymond Green", "Kevon Looney"]
   };
-  res.json(nbateams); 
+  // res.json(nbateams); 
 
-});
+// });
 
 app.get("/api/nbateams", (req, res) => {
   res.send(nbateams); 
@@ -86,8 +92,8 @@ app.post("/api/nbateams", (req, res) => {
     arena: req.body.arena,
     bestPlayer: req.body.bestPlayer, 
     titlesWon: req.body.titlesWon, 
-    starting5: req.body.starting5.split(",")
-  }
+    starting5: req.body.starting5
+  };
 
   nbateams.push(team); 
   res.send(nbateams); 
@@ -100,7 +106,8 @@ const validateTeam = (team) => {
     city: Joi.allow(""), 
     arena: Joi.allow(""), 
     bestPlayer: Joi.allow(""), 
-    titlesWon: Joi.allow("")
+    titlesWon: Joi.allow(""),
+    starting5: Joi.allow(),
   });
 
   return schema.validate(team); 
